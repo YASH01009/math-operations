f7218975e031678c49a02278ff230a50636accc5
Yashwin <yashwinacad@gmail.com>
2020-08-08 12:48:52 +0530

    Started the project with four basic operations with two operands.

---
+++ basic-math/basic-math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+a = 5
+b = 6
+
+print(add(a,b))
+print(subtract(a,b))
+print(multiply(a,b))
+print(divide(a,b))
