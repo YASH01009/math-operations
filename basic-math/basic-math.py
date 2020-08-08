79f6e1640f4b082af4a54d4680033718c8acb603
YASH01009 <46935127+YASH01009@users.noreply.github.com>
2020-08-08 13:42:30 +0530

    Added the provision for the third number from user.

--- basic-math/basic-math.py
+++ basic-math/basic-math.py
@@ -12,6 +12,7 @@ def divide(a,b):
 
 a = int(input("Enter the first number : "))
 b = int(input("Enter the second number : "))
+c = int(input("Enter the third number : "))
 
 print("The results are ")
 print("sum : " + add(a,b))
